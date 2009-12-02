660c753ec4cf296e7757d0e03855c609cab5c37d
Shane Becker <veganstraightedge@gmail.com>
2009-12-02 15:45:11 -0800

    vanilla rails app

---
+++ config/environments/development.rb
@@ -0,0 +1,6 @@
+config.whiny_nils                                    = true
+config.cache_classes                                 = false
+config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
+config.action_controller.perform_caching             = false
+config.action_mailer.raise_delivery_errors           = false
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+ActionController::Base.session = {
+  :key    => '_razzledazzle_session',
+  :secret => '8666e0a21e9d16d41791be0f86200f374995b20b1f183d4ed864e51f52f4d75223e62e8fd1761fcb32aab0cb19ef43dbdeeb7deb6c2de1ce0c8ade525ae9b1f4'
+}
