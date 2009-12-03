77014ffee37e1f656bb1066496e3f179384c3569
Shane Becker <veganstraightedge@gmail.com>
2009-12-02 16:03:09 -0800

    infrastructure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => "comps", :action => "home"
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
