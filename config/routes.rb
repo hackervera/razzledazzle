16f2d61fd8c0c5d6ff19044813ba0c21097a76a4
unknown <tyler@.(none)>
2009-12-08 22:50:52 -1000

    added a bunch of openid stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :openid_users
+
   map.resources :activities
   map.root      :controller => "activities"
+  map.login     "/login", :controller => "openid_users", :action => "login"
+  map.signup    "/signup", :controller => "openid_users", :action => "signup"
   map.manifesto "/manifesto", :controller => "about", :action => "manifesto"
   map.things_that_matter "/things_that_matter", :controller => "about", :action => "things_that_matter"
   map.connect  ':controller/:action/:id'
